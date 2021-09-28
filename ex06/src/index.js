547cf6b2abfe2d8b68c164b1e94251f9f2ee3f08
arminpeco <apeco1906@gmail.com>
2021-09-29 00:48:24 +0200

    Added file index.js

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function firstName(firstName) {
+    return firstName.toUpperCase();
+}
+function firstName(lastName) {
+    return lastName.toLowerCase();
+}
+
+exports.lastName=lastName;
+exports.firstName=firstName;
